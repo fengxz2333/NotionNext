73ba0a6d1ce81267d1c08f1e4f6d44faccecb29b
fengxz2333 <67378182+fengxz2333@users.noreply.github.com>
2023-11-18 22:22:57 +0800

    Update blog.config.js
    数字艺术创作者

--- blog.config.js
+++ blog.config.js
@@ -13,12 +13,12 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Hi，我是一名动画导演, 曾参与过《流浪地球2》、《独行月球》《宇宙探索编辑部》等电影作品,欢迎来到我的博客🎉',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Hi，我是一名数字艺术创作者, 曾以Previs Artist身份参与过《流浪地球2》《独行月球》《宇宙探索编辑部》等电影作品',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'FENX', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '作者简介', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '个人简介', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://fenxz.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '个人作品集网站', // 网站关键词 英文逗号隔开
 
